bcbe150e34289a9e1a2ebfcff4851a8c8e816d62
adtsay <ad.tsay@outlook.com>
2021-11-06 09:39:57 +0500

    reset shortcut

--- Git-Commands.psm1
+++ Git-Commands.psm1
@@ -32,6 +32,8 @@ function Use-GitLog5 { git log --graph --abbrev-commit --decorate --format=oneli
 function Use-GitClean { git clean -df }
 function Use-GitShow { git show }
 function Use-GitShowF { git show --pretty='format:' --name-only }
+function Use-GitResetHard { git reset --hard }
+function Use-GitResetArgs { git reset $args }
 
 Set-Alias -Name gec -Value Use-GitEditConfig #git config --global -e
 Set-Alias -Name gs -Value Use-GitStatus #git status
@@ -41,7 +43,7 @@ Set-Alias -Name gpull -Value Use-GitPull #git pull
 Set-Alias -Name gpush -Value Use-GitPush #git push
 Set-Alias -Name gpushd -Value Use-GitPushD #git push -u origin HEAD
 Set-Alias -Name gf -Value Use-GitFetch #git fetch
-Set-Alias -Name gb -Value Use-GitBranch #git branch --show-current
+Set-Alias -Name gb -Value Use-GitBranch #git branch
 Set-Alias -Name gba -Value Use-GitBranchArgs #git branch $args
 Set-Alias -Name gsa -Value Use-GitSwitchBranch #git switch $branch
 Set-Alias -Name gsc -Value Use-GitSwitchC #git switch -c $branch
@@ -58,5 +60,7 @@ Set-Alias -Name glog -Value Use-GitLog #git log
 Set-Alias -Name glogn -Value Use-GitLogN #git log --graph --abbrev-commit --decorate --format=oneline -$n
 Set-Alias -Name glog5 -Value Use-GitLog5 #git log --graph --abbrev-commit --decorate --format=oneline -5
 Set-Alias -Name gclean -Value Use-GitClean #git clean -df
-Set-Alias -Name gsh -Value Use-GitShow #git show 
+Set-Alias -Name gsh -Value Use-GitShow #git show
 Set-Alias -Name gshf -Value Use-GitShowF #git show --pretty='format:' --name-only
+Set-Alias -Name grh -Value Use-GitResetHard #git reset --hard
+Set-Alias -Name gra -Value Use-GitResetArgs #git reset $args
