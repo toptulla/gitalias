362b051d9a222a2c9c371a229b2eab1ec81be8ab
adtsay <ad.tsay@outlook.com>
2021-11-06 14:40:03 +0500

    comments for aliases

--- Git-Commands.psm1
+++ Git-Commands.psm1
@@ -3,7 +3,7 @@ function Copy-ThisModule {
     Get-Item .\Git-Commands.psm1 |
         Copy-Item -Destination '~\Documents\PowerShell\Modules\Git-Commands\'
 }
-Set-Alias -Name ctm -Value Copy-ThisModule
+Set-Alias ctm Copy-ThisModule
 
 function Use-GitEditConfig { git config --global -e }
 function Use-GitStatus { git status }
@@ -35,32 +35,38 @@ function Use-GitShowF { git show --pretty='format:' --name-only }
 function Use-GitResetHard { git reset --hard }
 function Use-GitResetArgs { git reset $args }
 
-Set-Alias -Name gitec -Value Use-GitEditConfig #git config --global -e
-Set-Alias -Name gits -Value Use-GitStatus #git status
-Set-Alias -Name gitsi -Value Use-GitSubmoduleInit #git submodule init
-Set-Alias -Name gitsu -Value Use-GitSubmoduleUpdate #git submodule update
-Set-Alias -Name gitp -Value Use-GitPull #git pull
-Set-Alias -Name gitps -Value Use-GitPush #git push
-Set-Alias -Name gitpsd -Value Use-GitPushD #git push -u origin HEAD
-Set-Alias -Name gitf -Value Use-GitFetch #git fetch
-Set-Alias -Name gitb -Value Use-GitBranch #git branch
-Set-Alias -Name gitba -Value Use-GitBranchArgs #git branch $args
-Set-Alias -Name gitsw -Value Use-GitSwitchBranch #git switch $branch
-Set-Alias -Name gitswc -Value Use-GitSwitchC #git switch -c $branch
-Set-Alias -Name gitswb -Value Use-GitSwitchBack #git switch -
-Set-Alias -Name gitswd -Value Use-GitSwitchDevelop #git switch develop
-Set-Alias -Name gitswm -Value Use-GitSwitchMaster #git switch master
-Set-Alias -Name gitcm -Value Use-GitCommitM #git commit -m $message
-Set-Alias -Name gitcam -Value Use-GitCommitAM #git commit -am $message
-Set-Alias -Name gita -Value Use-GitAddAll #git add --all
-Set-Alias -Name gitaa -Value Use-GitAddArgs #git add &args
-Set-Alias -Name gitd -Value Use-GitDiff #git diff
-Set-Alias -Name gitdc -Value Use-GitDiffCached #it diff --cached
-Set-Alias -Name gitlog -Value Use-GitLog #git log
-Set-Alias -Name gitlogn -Value Use-GitLogN #git log --graph --abbrev-commit --decorate --format=oneline -$n
-Set-Alias -Name gitlog5 -Value Use-GitLog5 #git log --graph --abbrev-commit --decorate --format=oneline -5
-Set-Alias -Name gitcl -Value Use-GitClean #git clean -df
-Set-Alias -Name gitsh -Value Use-GitShow #git show
-Set-Alias -Name gitshf -Value Use-GitShowF #git show --pretty='format:' --name-only
-Set-Alias -Name gitrh -Value Use-GitResetHard #git reset --hard
-Set-Alias -Name gitra -Value Use-GitResetArgs #git reset $args
+Set-Alias gitec Use-GitEditConfig #gitec => git config --global -e
+Set-Alias gits Use-GitStatus #gits => git status
+Set-Alias gitsi Use-GitSubmoduleInit #gitsi => git submodule init
+Set-Alias gitsu Use-GitSubmoduleUpdate #gitsu => git submodule update
+Set-Alias gitp Use-GitPull #gitp => git pull
+Set-Alias gitps Use-GitPush #gitps => git push
+Set-Alias gitpsd Use-GitPushD #gitpsd => git push -u origin HEAD
+Set-Alias gitf Use-GitFetch #gitf => git fetch
+Set-Alias gitb Use-GitBranch #gitb => git branch
+Set-Alias gitba Use-GitBranchArgs #gitba => git branch $args
+Set-Alias gitsw Use-GitSwitchBranch #gitsw => git switch $branch
+Set-Alias gitswc Use-GitSwitchC #gitswc => git switch -c $branch
+Set-Alias gitswb Use-GitSwitchBack #gitswb => git switch -
+Set-Alias gitswd Use-GitSwitchDevelop #gitswd => git switch develop
+Set-Alias gitswm Use-GitSwitchMaster #gitswm => git switch master
+Set-Alias gitcm Use-GitCommitM #gitcm => git commit -m $message
+Set-Alias gitcam Use-GitCommitAM #gitcam => git commit -am $message
+Set-Alias gita Use-GitAddAll #gita => git add --all
+Set-Alias gitaa Use-GitAddArgs #gitaa => git add &args
+Set-Alias gitd Use-GitDiff #gitd => git diff
+Set-Alias gitdc Use-GitDiffCached #gitdc => git diff --cached
+Set-Alias gitlog Use-GitLog #gitlog => git log
+Set-Alias gitlogn Use-GitLogN #gitlogn => git log --graph --abbrev-commit --decorate --format=oneline -$n
+Set-Alias gitlog5 Use-GitLog5 #gitlog5 => git log --graph --abbrev-commit --decorate --format=oneline -5
+Set-Alias gitcl Use-GitClean #gitcl => git clean -df
+Set-Alias gitsh Use-GitShow #gitsh => git show
+Set-Alias gitshf Use-GitShowF #gitshf => git show --pretty='format:' --name-only
+Set-Alias gitrh Use-GitResetHard #gitrh => git reset --hard
+Set-Alias gitra Use-GitResetArgs #gitra => git reset $args
+
+function Get-GitAlias {
+    Get-Content ~\Documents\PowerShell\Modules\Git-Commands\Git-Commands.psm1
+        | Where-Object { $_ -like 'Set-Alias*' }
+        | Select-Object { $_.Substring($_.IndexOf('#')) }
+}
